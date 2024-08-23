384ada52763c3146c0e07bdca5a6844111aba65b
yongjinpark <yjp9603@gmail.com>
2024-08-23 18:18:59 +0900

    test: user controller 테스트코드 작성

---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,102 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.UserStatus;
+import com.example.demo.repository.UserEntity;
+import com.example.demo.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/user-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    void 사용자는_특정_유저의_정보를_개인정보는_소거된채_전달_받을_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("yjp9603@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("jaden"))
+                .andExpect(jsonPath("$.address").doesNotExist())
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+
+    @Test
+    void 사용자는_존재하지_않는_유저의_아이디로_api를_호출할_경우_404_응답을_받는다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/123456789"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Users에서 ID 123456789를 찾을 수 없습니다."));
+    }
+
+    @Test
+    void 사용자는_인증_코드로_계정을_활성화_시킬_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(
+                        get("/api/users/2/verify")
+                                .queryParam("certificationCode", "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab"))
+                .andExpect(status().isFound());
+        UserEntity user = userRepository.findById(2L).get();
+        assertThat(user.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    void 사용자는_인증_코드가_일치하지_않을_경우_권한_없음_에러를_내려준다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(
+                        get("/api/users/2/verify")
+                                .queryParam("certificationCode", "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaac"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void 사용자는_내_정보를_불러올_때_개인정보인_주소도_갖고_올_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(
+                        get("/api/users/me")
+                                .header("EMAIL", "yjp9603@gmail.com"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("yjp9603@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("jaden"))
+                .andExpect(jsonPath("$.address").value("Seoul"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+
+
+
+}
---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'yjp9603@gmail.com', 'jaden', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'yjp9603@gmail.com', 'jaden2', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
