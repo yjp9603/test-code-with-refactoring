a085e61ccece8b8eebf3f9698d0cdd574cfeace9
yongjinpark <yjp9603@gmail.com>
2024-08-29 23:04:53 +0900

    test: 유저정보조회 테스트추가

--- src/test/java/com/example/demo/user/controller/response/UserResponseTest.java
+++ src/test/java/com/example/demo/user/controller/response/UserResponseTest.java
@@ -1,4 +1,36 @@
 package com.example.demo.user.controller.response;
 
+import com.example.demo.post.controller.response.PostResponse;
+import com.example.demo.post.domain.Post;
+import com.example.demo.user.controller.reponse.UserResponse;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class UserResponseTest {
+
+    @Test
+    public void User으로_응답을_생성할_수_있다() {
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("yjp9603@gmail.com")
+                .nickname("jaden")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .lastLoginAt(100L)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                .build();
+
+        // when
+        UserResponse userResponse = UserResponse.from(user);
+
+        // then
+        assertThat(userResponse.getId()).isEqualTo(1);
+        assertThat(userResponse.getEmail()).isEqualTo("yjp9603@gmail.com");
+        assertThat(userResponse.getStatus()).isEqualTo(UserStatus.ACTIVE);
+        assertThat(userResponse.getLastLoginAt()).isEqualTo(100L);
+    }
 }
