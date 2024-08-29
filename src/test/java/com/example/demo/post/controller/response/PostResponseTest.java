585cae95b57d41cf1fd0b74afd8ce5035f525be7
yongjinpark <yjp9603@gmail.com>
2024-08-29 22:54:15 +0900

    test: post 응답 테스트 추가

--- src/test/java/com/example/demo/post/controller/response/PostResponseTest.java
+++ src/test/java/com/example/demo/post/controller/response/PostResponseTest.java
@@ -1,5 +1,35 @@
 package com.example.demo.post.controller.response;
 
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import org.junit.jupiter.api.Test;
+import com.example.demo.post.domain.Post;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PostResponseTest {
 
+    @Test
+    public void Post로_응답을_생성할_수_있다() {
+        // given
+        Post post = Post.builder()
+                .content("hello test")
+                .writer(User.builder()
+                        .email("yjp9603@gmail.com")
+                        .nickname("jaden")
+                        .address("Seoul")
+                        .status(UserStatus.ACTIVE)
+                        .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                        .build())
+                .build();
+
+        // when
+        PostResponse postResponse = PostResponse.from(post);
+
+        // then
+        assertThat(postResponse.getContent()).isEqualTo("hello test");
+        assertThat(postResponse.getWriter().getEmail()).isEqualTo("yjp9603@gmail.com");
+        assertThat(postResponse.getWriter().getNickname()).isEqualTo("jaden");
+        assertThat(postResponse.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
 }
